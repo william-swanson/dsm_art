12cd0d9c50634544376b17311ec81c63ab97cb7d
Clayton Brady <cjbrady09@gmail.com>
2017-10-29 15:05:38 -0500

    horizontal list help

---
+++ app/src/main/java/com/willard5991/dsmarthub/ArtPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.willard5991.dsmarthub;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.view.MotionEvent;
+
+/**
+ * Created by clayton on 10/29/17.
+ */
+
+//This is my pageradapter to get the tabs to work
+//If you've already done this elsewhere, feel free to remove
+public class ArtPagerAdapter  extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public ArtPagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                DiscoverFragment tab1 = new DiscoverFragment();
+                return tab1;
+            case 1:
+                DiscoverFragment tab2 = new DiscoverFragment();
+                return tab2;
+            case 2:
+                DiscoverFragment tab3 = new DiscoverFragment();
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
